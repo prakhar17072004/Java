516fbdfd995c079d7d368ecd0912c8c58e6ad61e
prakhar17072004 <pmankur2004@gmail.com>
2024-08-17 16:21:31 +0530

    file is added

--- Big.java
+++ Big.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
 class Big
 {
   public static void main(String[] args)
   {
+    Scanner sc =new Scanner(System.in);
+  int a =sc.nextInt();
+  int b =sc.nextInt();
+  int sum = getBig(a,b);
   
-  
+  System.out.println("Greater numberis is :"+sum);  
   }
   
-  public static getBig(int num1,int num2)
+  public static int getBig(int num1,int num2)
   {
-   if(num1>>num2)
+   if(num1 >num2)
    {
    return num1;
    }
@@ -16,3 +21,6 @@ class Big
    return num2;
    }
  }
+ 
+ }
+ 
